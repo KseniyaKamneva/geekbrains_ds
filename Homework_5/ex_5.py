6764995891b74b92c1fbdca9e3a5b9f1d12b82e1
Kseniya Kamneva <kseniya5390@gmail.com>
2022-05-26 01:00:51 +0300

    Fifth homework

---
+++ Homework_5/ex_5.py
@@ -0,0 +1,11 @@
+"""
+Created on Mon May 23 02:28:27 2022
+
+@author: Ксения Камнева
+"""
+from random import randint
+with open('5.txt', mode='w+', encoding='utf-8') as f:
+    f.write(" ".join([str(randint(1, 1000)) for _ in range(100000)]))
+    f.seek(0)
+    print(sum(map(int, f.readline().split())))
+    
